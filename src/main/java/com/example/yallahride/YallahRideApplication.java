4a525dda49f37cf893a24adadb8da6996468095f
hasanms97 <shnaqhassan@hotmail.com>
2023-04-27 16:59:12 +0300

    first commit

---
+++ src/main/java/com/example/yallahride/YallahRideApplication.java
@@ -0,0 +1,13 @@
+package com.example.yallahride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallahRideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YallahRideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yallahride/YallahRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yallahride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallahRideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
